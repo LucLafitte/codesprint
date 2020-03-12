92ff3b89fbcc61e7881f6a9c29fdcaa6eb829794
Luc <lafitte.luc19@gmail.com>
2020-03-12 15:20:55 +0100

    ajout match

--- app.js
+++ app.js
@@ -2,68 +2,94 @@ const express = require("express") // On appelle express
 const app = express()
 const axios = require("axios") // On appelle axios
 const bodyParser = require('body-parser');
-//yes   
+
+// Middleware
 app.set("view engine", "ejs")
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
+// Constantes
 API_KEY = "APetT84BNRPwmHtw"
 API_SECRET = "5klcbi9ORwzWGIbgyqJrzO3nFTU2BnzH"
 
 var stateForm = {
-    list: {
+    list:{
         countries:"",
-        leagues:"",
-        clubs: ""
+        teams:""
     },
     select:{
-        country: "",
-        league: "",
-        club:""
+        team: ""
+    }
+}
+
+var random = {
+    select:{
+        team: ""
     }
 }
 
 app.get("/", function (req, res){   // quand je suis a la racine j'exécute la fonction
     getCountries().then( res2 => {
         stateForm.list.countries = res2.data.data.country
-    }).then( () => {
-        getLeagues(21).then( res3 => {
-            stateForm.list.leagues = res3.data.data.league
-        }).then( () => {
-            getClubs(21).then( res4 => {
-                console.log(res4)
-            })
-        })
+        res.render("index", {stateForm: stateForm})
     })
-    res.render("index", {stateForm: stateForm})
 })
 
+app.post("/teams",function(req, res){
+    let countryID = req.body.country
+    getTeams(countryID).then(response => {
+        stateForm.list.teams = response.data.data.teams
+        res.render("team", {stateForm: stateForm})
+    })
+})
+
+app.post("/match", function(req, res){
+    stateForm.select.team = req.body.team
+    getRandomTeam().then(response => {
+        random.select.team = response.data.data.team // la
+        res.render("match", {stateForm: stateForm, random: random})
+    })
+})
+ 
 async function getCountries(){
     const listCountries = await axios.get(`http://livescore-api.com/api-client/countries/list.json?key=APetT84BNRPwmHtw&secret=5klcbi9ORwzWGIbgyqJrzO3nFTU2BnzH`)
     return listCountries
 }
-async function getLeagues(leagueID){
-    const listeLeagues = await axios.get(`http://livescore-api.com/api-client/leagues/list.json?key=APetT84BNRPwmHtw&secret=5klcbi9ORwzWGIbgyqJrzO3nFTU2BnzH&country=${leagueID}`)
-    return listeLeagues
-}
-async function getClubs(clubID){
-    const listClubs = axios.get(`http://livescore-api.com/api-client/club/list.json?key=APetT84BNRPwmHtw&secret=5klcbi9ORwzWGIbgyqJrzO3nFTU2BnzH&club=${clubID}`)
-    return listClubs
+async function getTeams(countryID){
+    const listeTeams = await axios.get(`http://livescore-api.com/api-client/teams/list.json?key=APetT84BNRPwmHtw&secret=5klcbi9ORwzWGIbgyqJrzO3nFTU2BnzH&country_id=${countryID}`)
+    return listeTeams
 }
 
-app.post("/country", function(req, res){
-    let countryFromID = req.body.country
-    console.log(countryFromID)
-})
-app.post("/league", function(req, res){
-    let leagueFromID = req.body.country
-    console.log(leagueFromID)
-})
-app.post("/club", function(req, res){
-    let clubFromID = req.body.country
-    console.log(clubFromID)
-})
+async function getRandomTeam(){
+    const allTeam = await axios.get(`http://livescore-api.com/api-client/teams/list.json?key=APetT84BNRPwmHtw&secret=5klcbi9ORwzWGIbgyqJrzO3nFTU2BnzH`)
+    aleatoireNb();
+    return allTeam[unNombre]; // et la
+}
+
+async function aleatoireNb() {
+    unNombre = (Math.floor((5818)*Math.random()+1));
+    return unNombre
+    }
+    
+
+    
+    
+
+// app.post("/country", function(req, res){
+//     let countryFromID = req.body.country
+//     let listeTeams = getTeams(countryFromID)
+//         .then(response => {
+//             stateForm.list.teams = response.data.data.teams
+//             res.render("team", {stateForm: stateForm})
+//         })
+// })
+// app.post("/league", function(req, res){
+//     let leagueFromID = req.body.country
+//     // console.log(leagueFromID)
+// })
+
+
 
 app.listen(3000)
--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,14 @@
-$("#selctCountry").on("change", function(){
-    let country = $(this).val()
-    $.ajax({
-        method: "POST",
-        url: "/country",
-        data: {country: country},
-        success: function(html, statut){
-            console.log(html)
-        },
-        complete : function(resultat, statut){
-            console.log(resultat)
-        }
-    })
-})
+// $("#selctCountry").on("change", function(){
+//     let country = $(this).val()
+//     $.ajax({
+//         method: "POST",
+//         url: "/country",
+//         data: {country: country},
+//         success: function(html, statut){
+//             console.log(html)
+//         },
+//         complete : function(resultat, statut){
+//             console.log(resultat)
+//         }
+//     })
+// })
